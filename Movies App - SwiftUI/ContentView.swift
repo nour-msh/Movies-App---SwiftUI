f5d6933ecfa071bfb4e4c0e510502d2dab80ce5f
nour-msh <mshawrabnour@gmail.com>
2024-11-03 17:01:35 +0200

    Initial Commit

---
+++ Movies App - SwiftUI/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Movies App - SwiftUI
+//
+//  Created by Nour on 03/11/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
